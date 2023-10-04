dee23d3a33966907ff823ca5252061ceb2801ffa
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-04 18:56:22 +0900

    [add]ページの作成のみ

---
+++ src/pages/post/create.tsx
@@ -0,0 +1,17 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+
+
+
+export default function Create() {
+    
+
+    return (
+      <>
+        <h1>作成ページ</h1>
+        
+        {/* fromを作成する メモの内容を入力するだけ*/}
+
+      </>
+    )
+  }
