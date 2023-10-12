107a36899768c7730ef4459c95fae24602cd11f9
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-12 19:05:05 +0900

    [add]詳細表示途中まで2

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,45 +1,45 @@
-import {useState,useEffect} from 'react';
+import { useRouter } from 'next/router';
+import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
-import Link from 'next/link';
-import {useRouter} from "next/router";
 
+// QueryClientのインスタンスを作成
+const queryClient = new QueryClient();
 
-
-
-type details={
-  id:string;
-  title:string;
-  content:string;
-}
+// データの型を定義
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
 
 export default function Details() {
-  const router=useRouter();
-  const {postId}=router.query;
-  const [details,setDetails]=useState<details>();
+  const router = useRouter();
+  const { postId } = router.query;
   const api = `http://localhost:18080/v1/note/${postId}`;
 
-  useEffect(()=>{
-    if(postId){
-      axios.get(api)
-      .then((res)=>{
-        console.log(res.data);
-        setDetails(res.data);
+  // useQueryフックを使用してデータを取得
+  const { data, isLoading, isError } = useQuery<Post>(`${postId}`, () => {
+    return axios.get<Post>(api)
+      .then((response) => {
+        return response.data;
       })
-      .catch((err)=>{
-        console.log("エラーが発生しました",err);
-      })
-    }
-  },[postId]);
-  
+      .catch((error) => {
+        // データの取得に失敗した場合のエラーハンドリング
+        throw new Error('データを取得できませんでした');
+      });
+  });
 
+  // ローディング中の場合
+  if (!data) return <p>Loading...</p>;
 
-
+  // データが正常に取得された場合
   return (
     <>
       <h1>データ表示</h1>
       <div>
-
+        <h2>{data.title}</h2>
+        <p>{data.content}</p>
       </div>
     </>
-  )
+  );
 }
