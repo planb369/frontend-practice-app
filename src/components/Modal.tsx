4228df40cdb434657f570dd4ab93f71fb1784355
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-18 19:03:13 +0900

    [add] 削除のデザイン途中まで

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import deleteModal from "../styles/deleteModal.module.css"
 
 type ModalProps = {
     showFlag: boolean;
@@ -22,8 +23,8 @@ const Modal: React.FC<ModalProps> = (props) => {
     return (
         <>
         {props.showFlag ? ( // showFlagがtrueだったらModalを表示する
-            <div id="overlay" style={overlay}>
-                <div id="modalContent" style={modalContent}>
+            <div className={deleteModal.overlay}>
+                <div className={deleteModal.modalContent}>
                 <p>削除しますか？</p>
                 <button onClick={closeModal}>閉じる</button>
                 <button onClick={onClickDelete}>削除</button>
@@ -38,21 +39,3 @@ const Modal: React.FC<ModalProps> = (props) => {
 };
 
 export default Modal;
-
-const modalContent: React.CSSProperties = {
-    background: "white",
-    padding: "10px",
-    borderRadius: "3px",
-};
-
-const overlay: React.CSSProperties = {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0,0,0,0.5)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-};
