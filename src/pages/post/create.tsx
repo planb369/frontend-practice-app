6687e975092dcff3ba3b5cd2ffe75fed07ac4247
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-19 17:03:24 +0900

    [add] バリデーションができた

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -29,66 +29,55 @@ export default function Create() {
     data
   ) => {
     try {
-      // バリデーションチェック
       const postData = { title: data.title, content: data.content };
       await axios.post(api, postData);
       console.log("成功しました");
       router.push("/");
-    } catch (validationErr) {
-      console.log("その他のエラー", validationErr);
+    } catch (err) {
+      console.log("データが送信できませんでした", err);
     }
   };
 
   return (
-    <>
-      <div className={create.container}>
-        <div className={create.indexBtn}>
-          <Link className={create.indexBtnText} href={"/"}>
-            一覧画面へ戻る
-          </Link>
-        </div>
-
-        <h1 className={create.h1}>作成ページ</h1>
-
-        <form className={create.form} onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <div>
-              <Controller
-                name="title"
-                control={control}
-                render={({ field }) => (
-                  <input
-                    className={create.titleErea}
-                    placeholder="タイトル"
-                    {...field}
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <Controller
-                name="content"
-                control={control}
-                render={({ field }) => (
-                  <textarea
-                    {...field}
-                    className={create.contentErea}
-                    placeholder="メッセージ"
-                  />
-                )}
-              />
-            </div>
-          </div>
-          <div className={create.createButtonContainer}>
-            <input
-              className={create.createButton}
-              type="submit"
-              value="送信"
-              disabled={false} // ボタンの状態をカスタマイズする場合に設定
-            />
-          </div>
-        </form>
+    <div className={create.container}>
+      <div className={create.indexBtn}>
+        <Link className={create.indexBtnText} href={"/"}>
+          一覧画面へ戻る
+        </Link>
       </div>
-    </>
+
+      <h1 className={create.h1}>作成ページ</h1>
+      <form className={create.form} onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <div className={create.formGroup}>
+            <label htmlFor="title">タイトル:</label>
+            <Controller
+              name="title"
+              control={control}
+              render={({ field }) => (
+                <input className={create.titleErea} type="text" {...field} />
+              )}
+            />
+            <p className={create.error}>{errors.title?.message}</p>
+          </div>
+          <div className={create.formGroup}>
+            <label htmlFor="content">メッセージ:</label>
+            <Controller
+              name="content"
+              control={control}
+              render={({ field }) => (
+                <textarea className={create.contentErea} {...field} />
+              )}
+            />
+            <p className={create.error}>{errors.content?.message}</p>
+          </div>
+        </div>
+        <div className={create.createButtonContainer}>
+          <button className={create.createButton} type="submit">
+            投稿
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
