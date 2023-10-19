572a26fdb2a8d9deae5024509c88016a5bc07477
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-19 17:06:15 +0900

    [add] createページのバリデーション

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -9,8 +9,14 @@ import React from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const errorScheme = yup.object().shape({
-  title: yup.string().required("タイトルは必須項目です"),
-  content: yup.string().required("メッセージは必須項目です"),
+  title: yup
+    .string()
+    .required("タイトルは必須項目です")
+    .max(120, "タイトルは120文字以内で入力してください"),
+  content: yup
+    .string()
+    .required("メッセージは必須項目です")
+    .max(100000, "メッセージは100000文字以内で入力してください"),
 });
 
 const api = "http://localhost:18080/v1/note";
