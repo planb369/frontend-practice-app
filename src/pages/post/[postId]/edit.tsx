8a898d849b92a79b59f65dbca26062dac99b3fde
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-19 13:56:16 +0900

    [add] editのバリデーション表示

--- src/pages/post/[postId]/edit.tsx
+++ src/pages/post/[postId]/edit.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 import Link from 'next/link';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { posts } from "../../../types"; 
 import React, { useState } from "react";
 import FeatchDetail from '@/apis/featchDetail';
@@ -87,18 +87,40 @@ export default function Edit() {
             <h1 className={create.h1}>編集ページ</h1>
 
             <form className={create.form} onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <div>
-                        <input className={create.titleErea} placeholder={data.title} {...register('title', { required: true })} />
-                        {errors.title && <p>タイトルを入力してください</p>}
-                    </div>
-                    <div>
-                        <textarea className={create.contentErea} placeholder={data.content} {...register('content', { required: true })} />
-                        {errors.content && <p>メッセージを入力してください</p>}
-                    </div>
-                </div>
-                <div className={create.createButtonContainer}><input className={create.createButton} type="submit" /></div>
-            </form>
+          <div>
+            <div>
+              <Controller
+                name="title"
+                control={control}
+                render={({ field }) => (
+                  <input
+                    className={create.titleErea}
+                    placeholder="タイトル"
+                    {...field}
+                  />
+                )}
+              />
+              {/* バリデーションエラーメッセージを表示 */}
+              {titleValidationErrors && <p className={create.validationMessage}>{titleValidationErrors}</p>}
+            </div>
+            <div>
+              <Controller
+                name="content"
+                control={control}
+                render={({ field }) => (
+                  <textarea
+                    className={create.contentErea}
+                    placeholder="メッセージ"
+                    {...field}
+                  />
+                )}
+              />
+              {/* バリデーションエラーメッセージを表示 */}
+              {contentValidationErrors && <p className={create.validationMessage}>{contentValidationErrors}</p>}
+            </div>
+          </div>
+          <div className={create.createButtonContainer}><input className={create.createButton} type="submit" /></div>
+        </form>
         </div>
         
     </>
