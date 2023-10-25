dd0b566777c2db36ba01d76b50974a1d86c7f469
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-25 14:50:02 +0900

    [edit] _app.tsxをフォーマット

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
-import {AppProps} from "next/app";
-import {QueryClient,QueryClientProvider} from 'react-query';
-import '../styles/globals.css'; 
+import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "../styles/globals.css";
 
-const queryClient=new QueryClient();
+const queryClient = new QueryClient();
 
-function MyApp({Component,pageProps}: AppProps){
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
     </QueryClientProvider>
-  )
+  );
 }
 
 export default MyApp;
