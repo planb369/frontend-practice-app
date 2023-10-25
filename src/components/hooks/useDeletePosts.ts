433ec3ba330895a0ea5bd0e902adc181cd520a22
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-25 11:06:42 +0900

    [edit] useMutationで投稿削除

--- src/components/hooks/useDeletePosts.ts
+++ src/components/hooks/useDeletePosts.ts
@@ -2,7 +2,9 @@ import React from "react";
 import ModalStyle from "./ModalStyle.module.css";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { useMutation } from "react-query";
 import Modal from "../Modal";
+import { AppRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 //modalの型定義
 type ModalProps = {
@@ -11,22 +13,31 @@ type ModalProps = {
   onDelete: () => void;
 };
 
+//削除機能
+const deletePost = async (postId: string) => {
+  const api = `http://localhost:18080/v1/note/${postId}`;
+  const res = await axios.delete(api);
+  return res.data;
+};
+
+//読み込まれる関数
 export const useDeletePost = () => {
   const router = useRouter();
   const { postId } = router.query;
 
+  //deletePost関数を呼び出し
+  const mutation = useMutation(() => deletePost(postId as string), {
+    onSuccess: () => {
+      console.log("成功しました");
+      router.push("/");
+    },
+    onError: (err) => {
+      console.log("削除に失敗しました");
+    },
+  });
+
   const onDelete = () => {
-    const api = `http://localhost:18080/v1/note/${postId}`;
-    axios
-      .delete(api)
-      .then(() => {
-        console.log("成功しました");
-        // indexへ遷移
-        router.push("../../");
-      })
-      .catch((err) => {
-        console.log("データ送信に失敗しました", err);
-      });
+    mutation.mutate();
   };
   return { onDelete };
 };
