1eb846569fbe1b9e31f885cd86dca0dc25431f3f
planb369 <146909711+planb369@users.noreply.github.com>
2023-10-26 12:14:26 +0900

    [add] createのコメント

--- src/pages/post/create/create.tsx
+++ src/pages/post/create/create.tsx
@@ -20,16 +20,18 @@ const createPost = async (postData: Omit<Posts, "id">) => {
 };
 
 export default function Create() {
-  //useFromから必要なものを取得
+  //control, handleSubmit, formState の分割代入してフォーム制御に必要なものを取得
   const {
     control,
     handleSubmit,
     formState: { errors },
   } = useForm({
+    //オプションとしてresolver プロパティを指定して、バリデーションを設定
     resolver: yupResolver(postsScheme),
   });
   const router = useRouter();
 
+  //useMutationは第一引数に実行する処理、第二引数に成功時、失敗時の処理を書く
   const mutation = useMutation(createPost, {
     onSuccess: () => {
       console.log("成功しました");
@@ -40,11 +42,17 @@ export default function Create() {
     },
   });
 
+  //SubmitHandlerはReact Hook Formで使用される型
+  //titleとcontentオブジェクトを受け取る
   const onSubmit: SubmitHandler<{ title: string; content: string }> = async (
+    //入力されたdataを引数に取る
     data
   ) => {
     try {
       const postData = { title: data.title, content: data.content };
+
+      //reactQueryのuseMutationフックで定義されたミューテーション関数を実行するためのもの
+      //mutation処理が入った変数を実行している。
       mutation.mutate(postData);
     } catch (err) {
       console.log("データが送信できませんでした", err);
@@ -64,13 +72,17 @@ export default function Create() {
         <div>
           <div className={create.formGroup}>
             <label htmlFor="title">タイトル:</label>
+
+            {/* reactHookFromと連携させるためにControllerコンポーネントを使用する */}
             <Controller
-              name="title"
-              control={control}
+              name="title" //deta.titleになる部分
+              control={control} //これでnameの追跡ができるようになるらしい
+              // render={({ field })で変更がReact Hook Formによって追跡され制御できるらしい
               render={({ field }) => (
                 <input className={create.titleErea} type="text" {...field} />
               )}
             />
+            {/* useFromから取得したerrorsオブジェクト */}
             <p className={create.error}>{errors.title?.message}</p>
           </div>
           <div className={create.formGroup}>
